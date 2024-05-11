9965a09293b65ea22d622c09be35812d89bcb72c
ChromaMaster <vladtel.dev@gmail.com>
2024-05-11 19:09:59 +0200

    feat(nvim): add neo-tree plugin
    This also disables netrw, the default nvim file explorer
    
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

---
+++ nvim/lua/chromamaster/plugins/neo-tree.lua
@@ -0,0 +1,42 @@
+-- Neovim plugin to manage the file system and other tree like structures.
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			symlink_target = {
+				enabled = true,
+			},
+			window = {
+				position = "left",
+				width = 40,
+				mappings = {
+					["-"] = "open_split",
+					["|"] = "open_vsplit",
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+				hijack_netrw_behavior = "open_current",
+			},
+		})
+
+		-- vim.keymap.set("n", "<C-b>", ":Neotree focus<CR>", { desc = "Toggle file browser" })
+		vim.keymap.set("n", "\\", ":Neotree focus<CR>", { desc = "Toggle file browser" })
+	end,
+}
