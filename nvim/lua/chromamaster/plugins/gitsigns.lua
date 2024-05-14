0772cf2dd85aadaca3ba9c62ad97b5e5743b7daa
ChromaMaster <vladtel.dev@gmail.com>
2024-05-14 18:28:27 +0200

    feat(nvim): enable gitsigns line blame by default
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

--- nvim/lua/chromamaster/plugins/gitsigns.lua
+++ nvim/lua/chromamaster/plugins/gitsigns.lua
@@ -1,12 +1,15 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
 	"lewis6991/gitsigns.nvim",
-	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
-	},
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			current_line_blame = true,
+		})
+	end,
 }
