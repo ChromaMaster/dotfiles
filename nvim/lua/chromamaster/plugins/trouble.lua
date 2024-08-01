f998e88afd784b117b45e4ad82f619443dc53e83
ChromaMaster <vladtel.dev@gmail.com>
2024-08-01 19:35:56 +0200

    feat(nvim): update trouble keybindings
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

--- nvim/lua/chromamaster/plugins/trouble.lua
+++ nvim/lua/chromamaster/plugins/trouble.lua
@@ -9,12 +9,18 @@ return {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
+			preview = {
+				type = "split",
+				relative = "win",
+				position = "right",
+				size = 0.3,
+			},
 		})
 
 		-- Ignore formatting to not format functions inside this
 		-- stylua: ignore start
-		vim.keymap.set("n", "<leader>tw", function() trouble.open("workspace_diagnostics") end, { desc = "[T]rouble [W]orkspace diagnostics" })
-		vim.keymap.set("n", "<leader>td", function() trouble.open("document_diagnostics") end, { desc = "[T]rouble [D]ocument diagnostics" })
+		vim.keymap.set("n", "<leader>tw", function() trouble.open("diagnostics") end, { desc = "[T]rouble [W]orkspace diagnostics" })
+		vim.keymap.set("n", "<leader>td", function() trouble.open({mode = "diagnostics", filter = {buf = 0}}) end, { desc = "[T]rouble [D]ocument diagnostics" })
 		vim.keymap.set("n", "<leader>tn", function() trouble.next({skip_groups = true, jump = true}) end, { desc = "[T]rouble [N]ext item" })
 		vim.keymap.set("n", "<leader>tp", function() trouble.previous({skip_groups = true, jump = true}) end, { desc = "[T]rouble [P]revious item" })
 		vim.keymap.set("n", "<leader>tq", function() trouble.close() end, { desc = "[T]rouble [Q]uit" })
