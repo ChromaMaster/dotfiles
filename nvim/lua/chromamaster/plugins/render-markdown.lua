08ed805de0a4acda07a42bf5c606b3541db197ee
ChromaMaster <vladtel.dev@gmail.com>
2024-09-10 16:04:12 +0200

    feat(nvim): add render-markdown plugin
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

---
+++ nvim/lua/chromamaster/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+--  Plugin to improve viewing Markdown files in Neovim
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("render-markdown").setup({})
+	end,
+}
