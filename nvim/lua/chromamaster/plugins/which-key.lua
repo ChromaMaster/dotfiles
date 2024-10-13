c2889f0f108ddb1688d7b337a4b7731f975e2d0d
ChromaMaster <vladtel.dev@gmail.com>
2024-10-14 01:41:29 +0200

    feat(nvim): update which-key config for v3
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

--- nvim/lua/chromamaster/plugins/which-key.lua
+++ nvim/lua/chromamaster/plugins/which-key.lua
@@ -5,14 +5,14 @@ return { -- Useful plugin to show you pending keybinds.
 		require("which-key").setup()
 
 		-- Document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]rouble diagnostics", _ = "which_key_ignore" },
+		require("which-key").add({
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>d", group = "[D]ocument" },
+			{ "<leader>r", group = "[R]ename" },
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>b", group = "[B]uffer" },
+			{ "<leader>t", group = "[T]rouble diagnostics" },
 		})
 	end,
 }
