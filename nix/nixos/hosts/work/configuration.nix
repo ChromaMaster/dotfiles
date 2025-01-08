79406dbd46a16bd602c9a6701ba817b2f0d08503
ChromaMaster <vladtel.dev@gmail.com>
2025-01-08 11:10:26 +0100

    feat(nix): downgrade zoom-us to 6.2.11.5069
    https://github.com/NixOS/nixpkgs/issues/371488
    
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

--- nix/nixos/hosts/work/configuration.nix
+++ nix/nixos/hosts/work/configuration.nix
@@ -11,6 +11,26 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     slack
-    zoom-us
+    # zoom-us
+    (pkgs.zoom-us.overrideAttrs {
+      version = "6.2.11.5069";
+      src = pkgs.fetchurl {
+        url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+        hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+      };
+    })
   ];
+
+  # nixpkgs.overlays = [
+  #   # Fixes the version of zoom to a working one
+  #   (final: prev: {
+  #     zoom-us = prev.zoom-us.overrideAttrs (old: {
+  #       version = "6.2.11.5069";
+  #       src = final.fetchurl {
+  #         url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+  #         hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+  #       };
+  #     });
+  #   })
+  # ];
 }
