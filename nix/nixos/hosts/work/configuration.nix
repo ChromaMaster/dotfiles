e0fa9948ab2e60b4fcfbceaf3c6b2dcc569b2632
ChromaMaster <vladtel.dev@gmail.com>
2025-03-03 19:13:27 +0100

    feat(nix): move hostname definition to specific configuration files
    Signed-off-by: Vlad Alexandru Teletin Tilimpea <dev@vateletin.es>

--- nix/nixos/hosts/work/configuration.nix
+++ nix/nixos/hosts/work/configuration.nix
@@ -8,6 +8,8 @@
     ../../modules/work.nix
   ];
 
+  networking.hostName = "nixos"; # Define your hostname.
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
