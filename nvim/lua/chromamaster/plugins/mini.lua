d9813809428542b748aceb343aa3846288a0f613
ChromaMaster <vladtel.dev@gmail.com>
2025-03-18 13:06:19 +0100

    feat(nvim): use lualine instead of mini.statusline
    Signed-off-by: Vlad Alexandru Teletin Tilimpea <dev@vateletin.es>

--- nvim/lua/chromamaster/plugins/mini.lua
+++ nvim/lua/chromamaster/plugins/mini.lua
@@ -17,38 +17,6 @@ return {
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
 
-		-- Move any selection in any direction
-		--
-		-- M-h - Move Left
-		-- M-j - Move Down
-		-- M-k - Move Up
-		-- M-l - Move Right
-		-- require("mini.move").setup()
-
-		-- Simple and easy statusline.
-		--  You could remove this setup call if you don't like it,
-		--  and try some other statusline plugin
-		local statusline = require("mini.statusline")
-		statusline.setup()
-
-		-- You can configure sections in the statusline by overriding their
-		-- default behavior. For example, here we disable the section for
-		-- cursor information because line numbers are already enabled
-		---@diagnostic disable-next-line: duplicate-set-field
-		statusline.section_location = function()
-			return ""
-		end
-
-		-- Override the section_filename. In this case it will show only the
-		-- relative file name, if motified and if read only
-		-- f S  Path to the file in the buffer, as typed or relative to current directory.
-		-- m F  Modified flag, text is "[+]"; "[-]" if 'modifiable' is off.
-		-- r F  Readonly flag, text is "[RO]".
-		---@diagnostic disable-next-line: duplicate-set-field
-		statusline.section_filename = function()
-			return "%f%m%r"
-		end
-
 		-- ... and there is more!
 		--  Check out: https://github.com/echasnovski/mini.nvim
 	end,
