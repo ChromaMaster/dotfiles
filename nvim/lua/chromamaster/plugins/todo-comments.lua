63f3cff2941552bd01f4b74df53f3ceb074ae468
ChromaMaster <vladtel.dev@gmail.com>
2025-06-02 14:22:29 +0200

    feat(nvim): update plugin config format todo-comments
    Signed-off-by: Vlad Alexandru Teletin Tilimpea <dev@vateletin.es>

--- nvim/lua/chromamaster/plugins/todo-comments.lua
+++ nvim/lua/chromamaster/plugins/todo-comments.lua
@@ -3,9 +3,7 @@ return {
 	"folke/todo-comments.nvim",
 	event = "VimEnter",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("todo-comments").setup({
-			signs = false,
-		})
-	end,
+	opts = {
+		signs = false,
+	},
 }
