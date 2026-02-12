c913d63466ae0ea79388f44f2c087cd481a6e495
ChromaMaster <dev@vateletin.es>
2026-02-12 14:57:30 +0100

    feat(nvim): update treesitter config
    Signed-off-by: Vlad Alexandru Teletin Tilimpea <dev@vateletin.es>

--- nvim/lua/chromamaster/plugins/nvim-treesitter.lua
+++ nvim/lua/chromamaster/plugins/nvim-treesitter.lua
@@ -5,38 +5,39 @@ return {
 	config = function()
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"bash",
-				"c",
-				"go",
-				"rust",
-				"zig",
-				"python",
-				"html",
-				"lua",
-				"markdown",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
+		local filetypes = {
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"bash",
+			"html",
+			"markdown",
+			"markdown_inline",
+			"diff",
+			"lua",
+			"luadoc",
+			"c",
+			"go",
+			"rust",
+			"zig",
+			"python",
+		}
+
+		require("nvim-treesitter").install(filetypes)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = filetypes,
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
-
 		-- Use tresitter for folding
-		vim.wo.foldmethod = "expr"
-		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		vim.opt.foldenable = false
-
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+		-- vim.wo.foldmethod = "expr"
+		-- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		-- vim.opt.foldenable = false
 	end,
+	opts = {
+		highlight = { enable = true },
+		indent = { enable = true },
+	},
 }
