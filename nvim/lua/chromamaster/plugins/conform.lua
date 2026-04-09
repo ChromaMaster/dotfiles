57d2f14f1e045f91898ab091fa16703fb6a82e3d
ChromaMaster <dev@vateletin.es>
2026-04-09 11:10:48 +0200

    feat(nvim): add FormatDisable/Enable (autoformat on save) command
    Signed-off-by: Vlad Alexandru Teletin Tilimpea <dev@vateletin.es>

--- nvim/lua/chromamaster/plugins/conform.lua
+++ nvim/lua/chromamaster/plugins/conform.lua
@@ -17,8 +17,13 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+
 			-- Disable autoformat on certain filetypes
-			local ignore_filetypes = { "markdown", "xml" }
+			local ignore_filetypes = { "markdown", "xml", "cmake", "proto" }
 			if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
 				return
 			end
@@ -57,5 +62,23 @@ return {
 		-- The conform formatexpr should fall back to LSP when no formatters are available, and fall back to
 		-- the internal default if no LSP clients are available. So it should be safe to set it globally.
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
 	end,
 }
